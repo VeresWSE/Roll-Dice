78275d1dc8fa2f490d0de478c5e1880e970bb389
Piotr Veres Wereszczynski <piotrw2.0@gmail.com>
2020-07-04 14:03:42 +0200

    comments deleted

--- Program.cs
+++ Program.cs
@@ -73,55 +73,7 @@ class MainClass
 
 
 
-            /*
-                        string choiseOfThePlayerOne = ReadLine();
-                        int choiseOfThePlayerOneInt;
-
-                        while (!Int32.TryParse(choiseOfThePlayerOne, out choiseOfThePlayerOneInt)
-                        || choiseOfThePlayerOneInt > 3
-                        || choiseOfThePlayerOneInt <= 0)
-                        {
-                            WriteLine("Podaj poprawna liczbe");
-                            choiseOfThePlayerOne = ReadLine();
-                        }
-
-                        WriteLine("[Gracz 2] Rzuć kośćmi");
-                        string choiseOfThePlayerTwo = ReadLine();
-                        int choiseOfThePlayerTwoInt;
-
-                        while (!Int32.TryParse(choiseOfThePlayerTwo, out choiseOfThePlayerTwoInt)
-                        || choiseOfThePlayerTwoInt > 3
-                        || choiseOfThePlayerTwoInt <= 0)
-                        {
-                            WriteLine("Podaj poprawna liczbe");
-                            choiseOfThePlayerTwo = ReadLine();
-                        }
-
-                        if (choiseOfThePlayerOne == choiseOfThePlayerTwo)
-                        {
-                            WriteLine("Remis");
-                        }
-                        else if ((choiseOfThePlayerOne == "1" && choiseOfThePlayerTwo == "3")
-                        || (choiseOfThePlayerOne == "2" && choiseOfThePlayerTwo == "1")
-                        || (choiseOfThePlayerOne == "3" && choiseOfThePlayerTwo == "2"))
-                        {
-                            WriteLine("Wygrywa pierwszy gracz");
-                        }
-                        else if ((choiseOfThePlayerOne == "3" && choiseOfThePlayerTwo == "1")
-                        || (choiseOfThePlayerOne == "1" && choiseOfThePlayerTwo == "2")
-                        || (choiseOfThePlayerOne == "2" && choiseOfThePlayerTwo == "3"))
-                        {
-                            WriteLine("Wygrywa drugi gracz");
-                        }
-                        WriteLine("czy chcesz zakonczyc żywot? [y]");
-                        string isExit = ReadLine();
-
-                        if (isExit == "y")
-                        {
-                            break;
-                        }
-                        Clear();
-                        */
+            
         }
     }
 }
